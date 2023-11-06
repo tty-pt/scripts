747b849979b632623364834dea05619583feefbc
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2023-11-06 08:04:11 +0000

    0.2.4

--- babel.config.js
+++ babel.config.js
@@ -13,11 +13,11 @@ module.exports = {
     "@babel/preset-react"
   ],
   plugins: [
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
     ["@babel/plugin-proposal-decorators", { legacy: true, loose: true }],
-    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
     ["@babel/plugin-transform-private-property-in-object", { loose: true }],
     ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-class-properties", { loose: true }],
   ]
 };
 
